387ee781807e5238e7ec54a640d77e21d6703e6a
Ropats <rohitcpathare@gmail.com>
2023-04-21 01:43:59 +0530

    Add commentary to main App

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   // @ts-nocheck
-
+  // imports
   import { Route, router } from "tinro";
   import Upload from "./pages/upload.svelte";
   import View from "./pages/view.svelte";
@@ -8,29 +8,37 @@
   import { profile } from "./store";
   import Navbar from "./components/navbar.svelte";
 
+  // scroll to top of page
   router.mode.hash();
   router.subscribe((_) => window.scrollTo(0, 0));
 </script>
 
-<Navbar />
 <main>
+  <!-- Renders Navbar component -->
+  <Navbar />
   <Route path="/">
+    <!-- Redirects user to Upload page if connected to app -->
     {#if $profile}
       <Upload />
+      <!-- else displays Start (landing) page -->
     {:else}
       <Start />
     {/if}
   </Route>
   <Route path="/upload">
+    <!-- Displays Upload page only if user is connected to app -->
     {#if $profile}
       <Upload />
+      <!-- else displays Start (landing) page -->
     {:else}
       <Start />
     {/if}
   </Route>
   <Route path="/view">
+    <!-- Displays View page only if user is connected to app -->
     {#if $profile}
       <View />
+      <!-- else displays Start (landing) page -->
     {:else}
       <Start />
     {/if}
