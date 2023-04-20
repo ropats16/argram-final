ea1caa2b730c09aba2850d2df7943c25807fc6af
Ropats <rohitcpathare@gmail.com>
2023-04-20 12:23:01 +0530

    Update source contract and deploy script

--- src/contracts/deploy.js
+++ src/contracts/deploy.js
@@ -1,49 +1,32 @@
 import { getAddress } from 'permawebjs/wallet';
 import fs from 'fs';
-import path from 'path';
-import { createTransaction } from 'permawebjs/transaction';
+import { DeployPlugin, ArweaveSigner } from 'warp-contracts-plugin-deploy';
 import { WarpFactory } from 'warp-contracts';
-import { DeployPlugin } from 'warp-contracts-plugin-deploy';
+import path from 'path';
 
 const warp = WarpFactory.forMainnet().use(new DeployPlugin());
 
-const __dirname = path.resolve();
-
 const key = JSON.parse(fs.readFileSync('wallet.json').toString());
 
-const contractSource = fs.readFileSync('src/contracts/handle.js', 'utf-8');
+const __dirname = path.resolve();
+
+console.log(path.join(__dirname, 'src/contracts/handle.js'));
+
+const contractSource = fs.readFileSync(path.join(__dirname, 'src/contracts/handle.js'), 'utf-8');
 const addr = await getAddress({ key, environment: 'mainnet' });
 
 const inputTags = [
-  { name: 'Content-Type', value: 'application/javascript' },
   {
     name: 'Init-State', value: JSON.stringify({
-      creator: addr,
       owner: addr,
     })
   },
 ];
 
-const tx = await createTransaction({ data: contractSource, key: key, environment: 'mainnet', type: 'data', options: { tags: inputTags, signAndPost: true } });
+const newSource = await warp.createSource({ src: contractSource }, new ArweaveSigner(key));
 
-console.log("Transaction result for contract source with permawebJS", tx);
+const newSrcId = await warp.saveSource(newSource);
 
-await warp.register(tx.transaction.id, "node2");
-
-if (tx.postedTransaction.status === 400) {
-  throw new Error('Not enough $AR in wallet to upload pst!');
-} else if (tx.postedTransaction.status === 200) {
-  console.log("Txn uploaded successfully", tx);
-} else {
-  throw new Error(tx.postedTransaction.statusText + ' while trying to upload!');
-}
-
-// id: 'rpS7iRgOIRKaxpi8PDsL3qnI8GGies29K4i6ep3UdJs' => PermawebJS txn
-
-// id: '80UmfNuSj4dVUXgb1ZcarQxcNoZTdFQd8lO2AEDQFrU' => PermawebJS txn for new contractSource
-
-// id: 'CKmCX9XX9Dde7qO4pW48jUxFlloEeARzqM89pTI3TnQ' => PermawebJS txn for new contractSource v2
-
-// id: 'FnypXbwkNPEoZWZRW4UW0NGEudQk0D1kXArpMRSfV14' => Arweave txn
+console.log("New Source Contract Id", newSrcId);
 
 
--- src/contracts/handle.js
+++ src/contracts/handle.js
@@ -1,13 +1,12 @@
 export async function handle(state, action) {
   const { input, caller } = action;
   const balances = state.balances;
-  if (action.input.function == "__init") {
-    state.owner = action.caller;
-  }
+
   // function to get owner
   if (input.function === 'getOwner') {
     return { result: state.owner }
   }
+
   // function to get balances
   if (input.function === "balance") {
     let target;
@@ -49,10 +48,19 @@ export async function handle(state, action) {
 
     return { state }
   }
+
   // function to update comments
   if (input.function === 'addComment') {
-    state.comments.push({ comment: input.comment, user: input.user });
+    state.comments.push({ comment: input.txnData.comment, username: input.txnData.username, id: action.caller });
     return { state };
+  }
+
+  // function to add likes
+  if (input.function === 'likePost') {
+    if (Object.keys(state.likes).includes(caller)) {
+      throw new ContractError('User has voted!')
+    }
+    state.likes[caller] = Object.keys(state.likes).length + 1;
   }
   throw new ContractError('input.function not found!')
 }
