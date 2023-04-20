524cf77de893e1c4a370d5a1c3828bcfc2012533
Ropats <rohitcpathare@gmail.com>
2023-04-21 00:22:36 +0530

    Add reference comments to queryAssets.js

--- src/lib/queryAssets.js
+++ src/lib/queryAssets.js
@@ -1,7 +1,11 @@
 import { prop, propEq, find, pluck, filter } from 'ramda';
 
+// function to fetch posts create from defined contract source
 export async function getAssetData() {
+  // initialising empty array
   let assets = []
+
+  // fetch request
   await fetch(`https://arweave.net/graphql`, {
     method: 'POST',
     headers: {
@@ -9,10 +13,13 @@ export async function getAssetData() {
     },
     body: JSON.stringify({ query: idQuery() })
   })
+    // response cleanup
     .then(res => res.json())
     .then(({ data }) => Object.values(data.transactions))
     .then(edges => edges.map((node) => {
       node.map((sub) => {
+        // creating object of relevant data for each asset and pushing to 'assets' array
+        // contains transaction id, web url for image, title, description, content type, topics (hashtags), post owner, timestamp
         assets.push({
           id: sub.node?.id,
           image: `https://arweave.net/${sub.node?.id}`,
@@ -26,9 +33,12 @@ export async function getAssetData() {
       }
       )
     }))
+
+  // returns 'assets' array on function call
   return assets
 }
 
+// query requesting posts referencing the defined contract source
 function idQuery() {
   return `
 query {
