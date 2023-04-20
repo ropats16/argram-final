e80353ec819a09f6aec5754b866718d63843ca6e
Ropats <rohitcpathare@gmail.com>
2023-04-20 23:45:34 +0530

    Add comments to comment component

--- src/components/comment.svelte
+++ src/components/comment.svelte
@@ -1,4 +1,5 @@
 <script>
+  // imports
   import {
     writeContractWOthent,
     readContractWOthent,
@@ -7,27 +8,32 @@
   import { take } from "ramda";
   import Deploy from "../dialogs/deploy.svelte";
   import Error from "../dialogs/error.svelte";
-  import Confirm from "../dialogs/confirm.svelte";
   import { onMount } from "svelte";
 
+  // id variable to get the transaction id of an asset (post) from the view page
   export let id = "";
 
+  // object to handle users trying to add comments on multiple posts
   let comments = {};
 
+  // array storing the comments information for a post
   let commentsArray = [];
 
+  // pop dialog boxes for status updates
   let deployDlg = false;
   let errorMessage = "";
   let errorDlg = false;
-  let confirmDlg = false;
 
-  let tx = "";
-
+  // function to add comment
   async function addComment(e) {
     try {
+      // opens pop up showing posting in progress
       deployDlg = true;
 
+      // function to call `addComment` function from post's contract
+      // sends function name, username and comment text as inputs
       const res = await writeContractWOthent({
+        // function name to indicate writing to a contract
         othentFunction: "sendTransaction",
         data: {
           toContractId: id,
@@ -42,32 +48,31 @@
         },
       });
 
+      // closes pop up after successful comment addition and resets the input box in ui
       deployDlg = false;
-
       e.target.reset();
 
-      tx = res.transactionId;
-      confirmDlg = true;
-
+      // fetches the latest comments on a post
       commentsArray = await readComments();
-
-      confirmDlg = false;
     } catch (e) {
+      // error handling to display error in pop up
       deployDlg = false;
       errorMessage = e.message;
       errorDlg = true;
     }
   }
 
+  // reads the state of a post and returns the list of comments from it
+  // requires the post's transaction id (received from View Page in the 'id' var)
   async function readComments() {
     const res = await readContractWOthent({
       contractTxId: id,
     });
-    console.log("========================My contract state", res);
 
     return res.state["comments"];
   }
 
+  // fetches the latest list of comments for a post on the components first render
   onMount(async () => {
     commentsArray = await readComments();
   });
@@ -76,11 +81,15 @@
 <section
   class="hero pb-4 bg-base-100 flex flex-col border-solid border-2 border-slate-300 rounded-lg"
 >
+  <!-- Ensures commentsArray has comments -->
   {#if commentsArray.length > 0}
+    <!-- maps over elements of commentsArray -->
+    <!-- each element is given temp name 'comment' -->
     {#each commentsArray as comment}
       <p
         class="text-sm px-4 md:px-12 gap-2 flex flex-row items-center justify-start w-full"
       >
+        <!-- renders commenter's username or id -->
         <strong
           >{comment.username && comment.username != ""
             ? comment.username
@@ -89,30 +98,35 @@
       </p>
     {/each}
   {/if}
+  <!-- input form for calling 'addComment' function -->
   <form
     class="form px-4 md:px-12 mx-0 gap-2 flex flex-row items-center justify-center w-full"
     on:submit|preventDefault={addComment}
   >
     <div class="form-control w-full">
       <label for="comment" class="label">Comments</label>
+      <!-- input field for comment text -->
+      <!-- bind:value adds the comment text to the 'comments' variable -->
       <input
         id="comment"
         class="input input-bordered"
         bind:value={comments[id]}
         required
+        placeholder="Enter comment to enable button"
       />
       <p class="label text-sm text-gray-400">Enter a comment</p>
     </div>
+    <!-- button to submit comment form -->
+    <!-- button is disabled until valid comment is passed in -->
     <button disabled={!comments[id]} class="btn btn-block w-1/4">
       Comment
     </button>
   </form>
 </section>
+<!-- Error handling pop ups -->
 <Deploy open={deployDlg} />
 <Error
   open={errorDlg}
   msg={errorMessage}
   on:cancel={() => (errorDlg = false)}
 />
-
-<Confirm {tx} open={confirmDlg} on:cancel={() => (confirmDlg = false)} />
